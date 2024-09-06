1ad7081e8a197a152a57419d7c67c4beba8cbeec
beke.tamas <beke.tamas@BURG-1E06-19>
2024-09-06 08:11:03 +0200

    Add project files.

---
+++ BRICKS/Bricks.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BRICKS
+{
+    internal class Bricks
+    {
+        string itemID;
+        string itemName;
+        string CategoryName;
+        string ColorName;
+        int Qty;
+
+        public Bricks(string sor)
+        {
+            string[] tomb = sor.Split(';');
+            itemID = tomb[0];
+            itemName = tomb[1];
+            CategoryName = tomb[2];
+            ColorName = tomb[3];
+            Qty = int.Parse(tomb[4]);
+        }
+
+        public string ItemID  => itemID;
+        public string ItemName => itemName;
+        public string CategoryName1 => CategoryName;
+        public string ColorName1  => ColorName; 
+        public int Qty1  => Qty;
+    }
+}
---
+++ BRICKS/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace BRICKS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Bricks> list = [];
+        public MainWindow()
+        {
+            InitializeComponent();
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                XDocument document = XDocument.Load(ofd.FileName);
+                foreach (var elem in document.Descendants("Item"))
+                {
+                    list.Add(new Bricks($"{elem.Element("ItemID").Value};{elem.Element("ItemName").Value};{elem.Element("CategoryName").Value};{elem.Element("ColorName").Value};" +
+                        $"{elem.Element("Qty").Value}"));
+                }
+            }
+            MessageBox.Show($"{list.First().ItemName}");
+            dgBricks.ItemsSource = list;
+        }
+    }
+}
